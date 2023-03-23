131751428de0c214c38e6bf1c30ed46eaefc7083
Sunset <grigory.orlov.set@gmail.com>
2023-03-23 14:33:16 +0500

    DownloadAndRun.ps1 improvements

--- DownloadAndRun.ps1
+++ DownloadAndRun.ps1
@@ -1,6 +1,9 @@
+$ErrorActionPreference = "Stop"
+
 Remove-Item -Path $Env:TEMP\Installer.ps1 -Force -ErrorAction SilentlyContinue
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 $Parameters = @{
     Uri             = "https://raw.githubusercontent.com/SunsetTechuila/AM-Win10-Installer/main/Installer.ps1"
     UseBasicParsing = $true
@@ -9,3 +12,5 @@ $Parameters = @{
 Invoke-WebRequest @Parameters
 
 Start-Process -FilePath powershell -ArgumentList "-ExecutionPolicy Bypass -File $Env:TEMP\Installer.ps1" -Verb RunAs
+
+exit
