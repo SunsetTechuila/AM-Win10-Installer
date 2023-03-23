c9e9dd88bc018271258eb1610040fe3fc327999c
Sunset <grigory.orlov.set@gmail.com>
2023-03-23 14:34:39 +0500

    misc

--- DownloadAndRun.ps1
+++ DownloadAndRun.ps1
@@ -2,8 +2,6 @@ $ErrorActionPreference = "Stop"
 
 Remove-Item -Path $Env:TEMP\Installer.ps1 -Force -ErrorAction SilentlyContinue
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
 $Parameters = @{
     Uri             = "https://raw.githubusercontent.com/SunsetTechuila/AM-Win10-Installer/main/Installer.ps1"
     UseBasicParsing = $true
